62bc35adff04371a312589a1e96dbae615e0738a
caren <carenoliveirasilvapontes@gmail.com>
2022-06-21 18:54:01 -0500

    exercicio 3

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,10 @@
+const num1 = 2;
+const num2 = 4;
+const num3 = 6;
+if (num1> num2 && num1 >num3){
+    console.log(num1);
+}else if (num2>num3 && num2>num1){
+    console.log(num2);
+}else if (num3>num2 && num3>num1){
+    console.log(num3);
+}
