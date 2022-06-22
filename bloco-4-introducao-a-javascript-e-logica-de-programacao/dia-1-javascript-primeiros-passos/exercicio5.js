0e1d0f806eb9cb593f9677dffb08e1aef4f21909
caren <carenoliveirasilvapontes@gmail.com>
2022-06-21 19:17:57 -0500

    exercicio 5

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio5.js
@@ -0,0 +1,10 @@
+const lado1 = 80;
+const lado2 = 60;
+const lado3 = 40;
+if (lado1+lado2+lado3==180){
+    console.log(true);
+}else if (lado1+lado2+lado3!==180){
+    console.log(false);
+}else if (lado1+lado2+lado>0){
+    console.log('erro');
+}
