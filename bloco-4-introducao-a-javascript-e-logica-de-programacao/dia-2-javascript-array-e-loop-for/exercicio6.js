c15dfcfdecc6c1c67189c5e35c2d37347b9d9e82
caren <carenoliveirasilvapontes@gmail.com>
2022-06-22 21:58:31 -0500

    alteração nos comentados

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio6.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio6.js
@@ -1,13 +1,4 @@
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// let numImpar;
-// for (let index = 0; index < numbers.length; index += 1) {
-//   if (numbers[index] % 2 !== 0) {
-//     numImpar.push(numbers[index]);
-//   } else {
-//     console.log("nenhum valor ímpar encontrado");
-//   }
-// }
-// console.log("numeros impares", numImpar);
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numImpar = [];
 for (index = 0; index < numbers.length; index += 1) {
