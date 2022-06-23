2aa703e2232bc20b1d7f27e5ccd63126bf26856f
caren <carenoliveirasilvapontes@gmail.com>
2022-06-23 15:39:11 -0500

    treinando

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio9.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio9.js
@@ -1,12 +1,21 @@
+// let array = [
+//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+//   23, 24, 25,
+// ];
+// let numerosDiv = [];
+// let resultDivisao = [];
+// for (let index = 0; index < array.length; index += 1) {
+//   numerosDiv = array[index] / 2;
+//   resultDivisao.push(numerosDiv);
+// }
+// console.log(resultDivisao);
+
 let array = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
   23, 24, 25,
 ];
-let numerosDiv = [];
-let resultDivisao = [];
+let divisao = [];
 for (let index = 0; index < array.length; index += 1) {
-  numerosDiv = array[index] / 2;
-  resultDivisao.push(numerosDiv);
+  divisao.push(array[index] / 2);
 }
-console.log(resultDivisao);
-
+console.log(divisao);
