e0936df46de74d17100f555e38d57a21e4ffa350
caren <carenoliveirasilvapontes@gmail.com>
2022-06-23 09:45:05 -0500

    treinando

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio6.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio6.js
@@ -1,13 +1,25 @@
-
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numImpar = [];
+// for (index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 !== 0) {
+//     numImpar.push(numbers[index]);
+//  }
+// }
+//  if(numImpar.length===0){
+//     console.log('nenhum valor Impar')
+//  }else{
+//     console.log(numImpar.length)
+// }
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numImpar = [];
-for (index = 0; index < numbers.length; index += 1) {
+
+for (let index = 0; index < numbers.length; index += 1) {
   if (numbers[index] % 2 !== 0) {
-    numImpar.push(numbers[index]);
- }
+    numImpar.push(numbers[index])
+  }
 }
- if(numImpar.length===0){
-    console.log('nenhum valor Impar')
- }else{
-    console.log(numImpar.length)
+if (numImpar === 0) {
+  console.log("nenhum valor ímpar encontrado");
+} else {
+  console.log(numImpar.length);
 }
