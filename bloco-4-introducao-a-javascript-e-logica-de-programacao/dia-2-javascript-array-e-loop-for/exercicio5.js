2d4e40dded1563574717c61ed94d8e5a95eb1e0b
caren <carenoliveirasilvapontes@gmail.com>
2022-06-22 20:15:54 -0500

    exercicio5

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio5.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorNumero=0;
+for(index=0;index<numbers.length;index+=1){
+   
+    if  ( maiorNumero  >  numbers [ index ] ) {
+             maiorNumero  =  maiorNumero;
+         }  else {
+             maiorNumero  =  numbers [ index ];
+         }
+    } 
+    
+    console.log( 'O maior númer é' +  maiorNumero ) ;
+
