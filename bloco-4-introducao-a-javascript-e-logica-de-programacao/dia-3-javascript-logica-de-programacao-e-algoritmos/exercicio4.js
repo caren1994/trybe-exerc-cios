b0de2ef366f2cd8142ff4b02cfdef2497399fb40
caren <carenoliveirasilvapontes@gmail.com>
2022-06-25 17:05:07 -0500

    exercicio bonus

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio4.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio4.js
@@ -1,7 +1,11 @@
+
+// esse exercicio acha os impares com %2 !==0 e não os primos 
+// tenho que procurar como achar os primos e voltar aqui 
+
 let div = [];
 let maior= [];
 for (let index = 2; index <= 50; index += 1) {
-  if (index % 2 !== 0) {
+  if (index % 2 !== 0) { 
     div.push(index);
   }
  
@@ -16,3 +20,18 @@ for (let index = 0; index < div.length; index += 1){
   }
 }
 console.log(maior);
+
+
+// multiplicar todos os numeros dentro do array pelo proximo numero,sem alterar o array original
+
+const array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+let newArray = array
+
+for (let i = 0; i < newArray.length; i++) {
+  let nextElement = newArray[i + 1]
+  const multiplica = !nextElement ? 2 : nextElement
+  newArray[i] *= multiplica;
+}
+console.log(newArray)
