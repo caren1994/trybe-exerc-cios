3b5b0aa510f59641e1b2bb4061b01acf077ad023
caren <carenoliveirasilvapontes@gmail.com>
2022-06-25 16:15:18 -0500

    exercicio3

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio3.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio3.js
@@ -1,18 +1,17 @@
 let guardaMaior = [];
-let guardaMenor=['java'];
+let guardaMenor='java';
 let array = ["java", "javascript", "python", "html", "css"];
 for (let index = 0; index < array.length; index += 1) {
-  if (guardaMaior.length > array[index].length) {
-    guardaMaior = guardaMaior;
-  } else {
+  if (guardaMaior.length < array[index].length) {
     guardaMaior = array[index];
-  }
+  } 
 }
 console.log(guardaMaior);
 
 for (let index1 = 0; index1 < array.length; index1 += 1) {
   if (guardaMenor.length > array[index1].length) {
-    guardaMenor = array[index1].length;
+    guardaMenor = array[index1];
   }
-}
+  }
+
 console.log(guardaMenor);
