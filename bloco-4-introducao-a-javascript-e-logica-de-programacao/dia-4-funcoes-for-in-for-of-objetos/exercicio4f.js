888aa74e5175fce9e304337128055efd367d7624
caren <carenoliveirasilvapontes@gmail.com>
2022-06-26 14:11:43 -0500

    exercicio4f terminado

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-funcoes-for-in-for-of-objetos/exercicio4f.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-funcoes-for-in-for-of-objetos/exercicio4f.js
@@ -4,8 +4,9 @@ function maior(param1){
 for (let index=0;index < nomes.length;index+=1){
     if(guardaMaior.length < nomes[index].length){
     guardaMaior = nomes[index];
-    return guardaMaior;
+   
     }
-}
+} return guardaMaior;
 }
 console.log(maior(nomes));
+
