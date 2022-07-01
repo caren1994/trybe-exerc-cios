9c2edd09200a888c492885af6cf4a5738b4c9f74
caren <carenoliveirasilvapontes@gmail.com>
2022-07-01 10:19:56 -0500

    exercicios

--- bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -16,6 +16,7 @@ const pai2=document.body.lastElementChild;
 terceiro.className='center-content';
 console.log(terceiro);
 pai2.appendChild(terceiro);
+terceiro.parentElement.style.backgroundColor='green';
 
 
 // exercicio4
@@ -34,9 +35,43 @@ pai2.appendChild(quinto);
 const sexto=document.createElement('section');
 sexto.className='right-content';
 pai2.appendChild(sexto);
+sexto.style.marginRight='auto';
 
-// exercicio7
-const pai4=document.getElementClassName('left-content');
-const setimo=document.createElement('img src=https://picsum.photos/200');
+// // exercicio7
+const pai4=document.querySelector('.left-content');
+const setimo=document.createElement('img');
+setimo.setAttribute('src','https://picsum.photos/200');
 setimo.className='small-image';
 pai4.appendChild(setimo);
+
+
+// exercicio8
+const pai5=document.querySelector('.right-content');
+const oitavo=document.createElement('ul');
+pai5.appendChild(oitavo);
+const array=['um','dois','tês','quatro','cinco','seis','sete','oito','nove','dez'];
+for(const number of array){
+    const li=document.createElement('li');
+    li.innerText=number;
+oitavo.appendChild(li); 
+}
+
+// exercicio9
+for (let index=0;index<3;index+=1){
+    const tagh3=document.createElement('h3');
+    tagh3.innerText='h3';
+    tagh3.className='description';
+    pai2.appendChild(tagh3);
+}
+// exercicio10
+primeiro.className='title';
+
+pai2.removeChild(quinto);
+terceiro.parentElement.style.backgroundColor='green';
+
+
+// const ultimape=document.
+// oitavo.removeChild();
+(oitavo.lastChild.remove());
+(oitavo.lastChild.remove());
+
